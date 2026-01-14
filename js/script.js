0ce06dfec568a1dff312d78408337b06f9f1c975
Harini <149151126+Harini1606@users.noreply.github.com>
2026-01-14 16:07:41 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
 
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+  const error = document.getElementById("errorMsg");
+
+  if (name === "" || email === "" || message === "") {
+    error.style.color = "red";
+    error.innerText = "All fields are required";
+    return;
+  }
+
+  error.style.color = "green";
+  error.innerText = "Message sent successfully!";
+});
